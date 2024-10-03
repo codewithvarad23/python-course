063212edf25668f5bcc1981916ba601f9be2fe6f
codewithvarad23 <codewithvarad@gmail.com>
2024-10-03 12:37:39 +0530

    python dictionary

--- dict-python .py
+++ dict-python .py
@@ -1,4 +1,16 @@
-d={
-    name:'varad',
-    age:21
+# Dictionaries are used to store data values in key:value pairs.
+# A dictionary is a collection which is ordered*, changeable and do not allow duplicates.
+# 
+
+dict1={
+    'name':'varad',
+    'lastname':'pandit',
+    'age':23
 }
+print(dict1)
+print(type(dict1))
+print(dict1['name'])
+print(dict1['lastname'])
+print(dict1)
+print(dict1.keys()) # all keys print
+print(dict1.values()) # all values print
